4fa58292e6c465af9948f9f6232e1950b7685c19
Stanislav <s.propisnov@trendline.in.ua>
2018-04-04 22:01:34 +0300

    s

--- Lessons20/src/SQLUtil.java
+++ Lessons20/src/SQLUtil.java
@@ -29,18 +29,22 @@ public class SQLUtil {
     }
     public  static void executeUpdateQuery(String sql)
     {
+
+
+
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjava", "root", "");
-             Statement statement =  connection.createStatement()
-
-        )
-        {
-
+             Statement statement = connection.createStatement();
+            ) {
+//            while (resultSet.next()) {
+//                String first_name = resultSet.getNString(1);
+//                String last_name = resultSet.getNString(2);
+//              Date hire_date = resultSet.getDate("hire_date");
+//               employees.add(new Employee(first_name,last_name,hire_date));
+//            }
+//            for(int i=0;i<200;i++){
+//                System.out.println(employees.get(i));
+//            }
             statement.executeUpdate(sql);
-            //executeUpdate(sql);
-          //  ResultSet resultSet = statement.executeUpdate(sql);
-
-
-
         }catch (SQLException e){
             e.printStackTrace();
         }

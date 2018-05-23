a420064c5b5049bd1ea80125c1a71dc1124da38c
Stanislav <s.propisnov@trendline.in.ua>
2018-05-23 21:14:34 +0300

    s

--- Wraper/src/Main.java
+++ Wraper/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Lable testlable = new Pizza("Four Seasons Pizza");
         testlable = new RegularExtra("Pepperoni",testlable);
+        testlable = new TooExtra("Tomato",testlable);
         System.out.println(testlable.getLabel());
     }
 }
---
+++ Wraper/src/TooExtra.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Станислав on 23.05.2018.
+ */
+public class TooExtra extends Extra {
+    public TooExtra(String labels, Lable lable) {
+        super(labels, lable);
+    }
+}
